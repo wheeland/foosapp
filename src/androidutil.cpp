3e5454603ee895dc6bc646961cfc78e26327826b
Wieland Hagen <wieland.hagen@kdab.com>
2019-11-02 21:54:36 +0100

    AndroidUtil: add logging

--- src/androidutil.cpp
+++ src/androidutil.cpp
@@ -3,20 +3,26 @@
 #include <QDir>
 #include <QFile>
 #include <QFileInfo>
+#include <QDebug>
 
 static const QString APPNAME("de.wheeland.foos");
 
 static bool assertDirCreated(const QString &top, const QString &name)
 {
     const QDir topDir(top);
-    if (!topDir.exists())
+    if (!topDir.exists()) {
+        qWarning() << topDir.absolutePath() << "doesn't exist";
         return false;
+    }
 
     const QFileInfo subDir(topDir, name);
     if (subDir.exists() && subDir.isDir())
         return true;
 
-    return topDir.mkdir(name);
+    const bool create = topDir.mkdir(name);
+    if (!create)
+        qWarning() << "Can't create" << topDir.absolutePath() << name;
+    return create;
 }
 
 bool AndroidUtil::createAppStorageDir()
