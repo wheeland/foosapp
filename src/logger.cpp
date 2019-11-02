691745a6e34aa3367e6e8e1ba5a7f33474707958
Wieland Hagen <wieland.hagen@kdab.com>
2019-11-03 00:20:02 +0100

    add missing logger classes

---
+++ src/logger.cpp
@@ -0,0 +1,67 @@
+#include "logger.h"
+
+static Logger *s_handlingLogger = nullptr;
+
+static void messageHandler(QtMsgType type, const QMessageLogContext &ctx, const QString &msg)
+{
+    s_handlingLogger->add(msg);
+}
+
+void Logger::install()
+{
+    s_handlingLogger = this;
+    qInstallMessageHandler(&messageHandler);
+}
+
+int Logger::lineCount() const
+{
+    return m_lineCount;
+}
+
+void Logger::setLineCount(int lineCount)
+{
+    if (m_lineCount == lineCount)
+        return;
+
+    beginResetModel();
+    m_lineCount = lineCount;
+    endResetModel();
+    emit lineCountChanged(m_lineCount);
+}
+
+void Logger::add(const QString &line)
+{
+    beginResetModel();
+    m_lines << line;
+    endResetModel();
+}
+
+int Logger::rowCount(const QModelIndex &parent) const
+{
+    return qMin(m_lines.size(), m_lineCount);
+}
+
+QVariant Logger::data(const QModelIndex &index, int role) const
+{
+    const int idx = index.row();
+
+    if (idx < 0 || idx >= rowCount(QModelIndex())) {
+        return QVariant();
+    }
+
+    const int linesIdx = m_lines.size() - 1 - idx;
+
+    switch (role) {
+    case TextRole:
+        return QVariant::fromValue(m_lines[linesIdx]);
+    default:
+        return QVariant();
+    }
+}
+
+QHash<int, QByteArray> Logger::roleNames() const
+{
+    QHash<int, QByteArray> ret;
+    ret[TextRole] = "text";
+    return ret;
+}
---
+++ src/logger.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QStringList>
+#include <QAbstractListModel>
+
+class Logger : public QAbstractListModel
+{
+    Q_OBJECT
+    Q_PROPERTY(int lineCount READ lineCount WRITE setLineCount NOTIFY lineCountChanged)
+
+    enum Role {
+        TextRole = Qt::UserRole + 1
+    };
+
+public:
+    using QAbstractListModel::QAbstractListModel;
+    ~Logger() override = default;
+
+    int rowCount(const QModelIndex &parent) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    void install();
+
+    void add(const QString &line);
+
+    void setLineCount(int lineCount);
+    int lineCount() const;
+
+signals:
+    void lineCountChanged(int lineCount);
+
+private:
+    QStringList m_lines;
+    int m_lineCount = 256;
+};
